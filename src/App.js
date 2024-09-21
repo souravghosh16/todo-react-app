74efd2569d2d900e291218f122fbd8eb5c7201ab
sourav-navsoft <sourav.ghosh@navsoft.in>
2024-09-21 08:09:13 +0530

    Revert "removed user reducer"
    This reverts commit 9542965bfa1c89183e7a76df922634cd31d390e1.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import { increment, decrement } from './reducers';
 
 function App() {
   const count = useSelector((state) => state.reducer.value);
+  const {name, address} = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   return (
       <div>
-          <h1>Count: user{count}</h1>
+          <h1>Count: user{count} {name} {address} </h1>
           <button onClick={() => dispatch(increment())}>+</button>
           <button onClick={() => dispatch(decrement())}>-</button>
       </div>
